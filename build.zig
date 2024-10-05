4ef006cb16f758bb6c3a55b4e96e24c94923b6cd
Tim Culverhouse <tim@timculverhouse.com>
2024-10-04 20:44:32 -0500

    devlog: add rss

--- build.zig
+++ build.zig
@@ -4,7 +4,7 @@ const zine = @import("zine");
 pub fn build(b: *std.Build) !void {
     zine.website(b, .{
         .title = "Tim Culverhouse",
-        .host_url = "https://zine-ssg.io",
+        .host_url = "https://culver.house",
         .layouts_dir_path = "layouts",
         .content_dir_path = "content",
         .assets_dir_path = "assets",
